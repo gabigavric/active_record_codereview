d45624ee1e102b111886707c42bfb12133d0cbe4
gabrijela <gabigavric@gmail.com>
2017-09-29 18:13:07 -0700

    wire the store and brands pages without any ruby code yet just static code

--- app.rb
+++ app.rb
@@ -3,3 +3,19 @@ require 'pry'
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/brands')do
+  erb(:brands)
+end
+
+get('/stores')do
+  erb(:stores)
+end
+
+get('/add_store')do
+  erb(:add_store)
+end
+
+get('/add_brand')do
+  erb(:add_brand)
+end
