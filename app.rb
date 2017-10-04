2131d973f6668c1c6e439a55786da25d1a3e0025
gabrijela <gabigavric@gmail.com>
2017-10-04 00:01:04 -0700

    delete shoe list page

--- app.rb
+++ app.rb
@@ -4,9 +4,6 @@ Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-get('/shoes')do
-  erb(:shoes)
-end
 
 get('/')do
   erb(:stores)
@@ -28,14 +25,8 @@ get('/delete_store')do
   erb(:stores)
 end
 
-get('/delete_shoe')do
-  erb(:shoes)
-end
 
 get('/edit_store')do
   erb(:edit_store)
 end
 
-get('/update_shoe')do
-  erb(:shoes)
-end
