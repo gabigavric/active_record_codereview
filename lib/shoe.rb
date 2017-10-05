f4b4942341931d59b5ba604ef9571189952784b0
Armin Sedghi <arminsedghi@gmail.com>
2017-10-04 23:44:30 -0700

    capitalized first letter on save

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,4 +3,10 @@ class Shoe < ActiveRecord::Base
   has_many :stores, through: :inventories
   validates_uniqueness_of :name
   validates(:name, {:length => {:maximum => 100}, :presence => true})
+  before_save(:capitalize_name)
+
+  private
+  def capitalize_name
+    self.name = (name.split(" ").map {|word| word.capitalize}).join(' ')
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,12 @@ class Store < ActiveRecord::Base
   has_many :shoes, through: :inventories
   validates_uniqueness_of :name
   validates(:name, {:length => {:maximum => 100}, :presence => true})
+  before_save(:capitalize_name)
+
+
+  private
+  def capitalize_name
+    self.name = (name.split(" ").map {|word| word.capitalize}).join(' ')
+  end
+
 end
