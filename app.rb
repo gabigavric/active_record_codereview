eb4eb76abbf7517a95a147b856c529cd0fb6edd2
Armin Sedghi <arminsedghi@gmail.com>
2017-10-04 21:30:19 -0700

    Can now list shoes in store

--- app.rb
+++ app.rb
@@ -6,9 +6,21 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 
 get('/create_fake_data') do
-  Store.create({:name => "Store Washington Square"})
-  Store.create({:name => "Store Union Square"})
-  Store.create({:name => "Store Alabama Square"})
+  store1 = Store.create({:name => "Store1"})
+  store2 = Store.create({:name => "Store2"})
+  store3 = Store.create({:name => "Store3"})
+  shoe1 = Shoe.create({:name => "Nike", :price => 100})
+  shoe2 = Shoe.create({:name => "Nike1", :price => 101})
+  shoe3 = Shoe.create({:name => "Nike2", :price => 102})
+  shoe4 = Shoe.create({:name => "Nike2", :price => 100})
+  store1.inventories.create({:store => store1, :shoe => shoe1})
+  store1.inventories.create({:store => store1, :shoe => shoe2})
+  store1.inventories.create({:store => store1, :shoe => shoe3})
+  store1.inventories.create({:store => store1, :shoe => shoe4})
+  store2.inventories.create({:store => store2, :shoe => shoe1})
+  store2.inventories.create({:store => store2, :shoe => shoe2})
+  store3.inventories.create({:store => store3, :shoe => shoe3})
+  store3.inventories.create({:store => store3, :shoe => shoe4})
   redirect('/')
 end
 
@@ -29,7 +41,7 @@ post('/edit_store')do
   else
      Store.create({:name => params['store_name']})
   end
-  redirect('/')
+  redirect '/'
 end
 
 get('/add_store')do
@@ -43,7 +55,7 @@ end
 
 post('/add_shoe')do
   Shoe.create({:name => params['shoe_name'],:price => params['price']})
-  redirect('/')
+  redirect '/'
 end
 
 get('/individual_store/:store_id')do
@@ -55,12 +67,13 @@ end
 post('/individual_store')do
   store = Store.find(params["store_id"])
   store.inventories.create({:store => store, :shoe => Shoe.find(params["shoe_id"])})
+  redirect "/individual_store/#{store.id}"
 
 end
 
 get('/delete_store/:id')do
   Store.destroy(params[:id])
-  redirect('/')
+  redirect '/'
 end
 
 
