f085e180528467c7dcddfe5293aec329a8473d53
Armin Sedghi <arminsedghi@gmail.com>
2017-10-04 22:49:46 -0700

    integration spec for add new store

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,1 +1,17 @@
-require 'spec_helper'
+require "capybara/rspec"
+require "./app"
+require "pry"
+require('spec_helper')
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+
+describe 'the store creation path', {:type => :feature} do
+  it 'takes the user to the homepage where they can add a store' do
+    visit '/'
+    click_link('Add a new Store')
+    expect(page).to have_content('Store Info')
+  end
+end
+
